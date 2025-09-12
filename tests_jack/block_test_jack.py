f3c226647add4c08f3c5af505b1a9c50c6e2ac5f
jackliu <jackliu@pop-os.localdomain>
2025-09-12 10:04:07 -0400

    updated code

---
+++ tests_jack/block_test_jack.py
@@ -0,0 +1,65 @@
+import pddl
+import json
+import os
+from dotenv import load_dotenv
+
+import pdb
+
+from pddl_planner.planner.nl_planner import NLFOLRegressionPlanner
+
+if __name__ == "__main__":
+    env_flag = False
+    try:
+        load_dotenv()
+        env_flag = True
+    except Exception as e:
+        print(f"Error loading .env file: {e}")
+    if env_flag:
+        print("Environment variables loaded successfully")
+    else:
+        print("Environment variables not loaded")
+        
+    # load the domain from the file
+    with open('./blockworld_domain.json', 'r') as f:
+        domain = json.load(f)
+
+    # load the goal blocks from the file    
+    with open('./instance-10.json', 'r') as f:
+        block_dict = json.load(f)
+
+    test_goal = block_dict["goal"]
+    test_init = block_dict["initial_state"]
+
+    # pdb.set_trace()
+
+    # create a directory to store the results
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    save_path = os.path.join(current_dir, f'alfworldtext_results')
+    os.makedirs(save_path, exist_ok=True)
+
+    # initialize the planner
+    for i, block in enumerate(test_goal):
+        print(f'Problem {block} =========================================')
+        planner = NLFOLRegressionPlanner(domain.copy(), test_goal.copy(), max_depth=3)
+        regressed_plans = planner.regress_plan()
+        # create a empty text file to store the results
+        save_file_path = os.path.join(save_path, f'alfworldtext_results_{i}.txt')
+        with open(save_file_path, 'w') as f:
+            # print the regressed plans
+            print("Regressed goals:")
+            f.write("Regressed goals:\n")
+            for plan in regressed_plans:
+                print("Subgoal: ")
+                f.write("Subgoal: \n")
+                print(plan[0])
+                f.write(str(plan[0]) + '\n')
+                reversed_plan = plan[1]
+                reversed_plan.reverse()
+                print("Action: ", reversed_plan)
+                f.write(str(reversed_plan) + '\n')
+                print("Substitution: ", plan[2])
+                f.write(str(plan[2]) + '\n')
+                print("--------------------")
+                f.write("--------------------\n")
+        break
+    #
